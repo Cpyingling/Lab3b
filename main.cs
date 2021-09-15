c6d38e30735f66543b0f6cc68b57ec9e7a49ef5a
Charlie Y <89798338+Cpyingling@users.noreply.github.com>
2021-09-15 03:31:29 +0000

    Changed comments

--- main.cs
+++ main.cs
@@ -1,15 +1,5 @@
 /*Author Charles Yingling     Date 9/14/2021
 2. Write a program in C# to calculate commission based on the SalesAmount.
-
-
-Based on the sales amount, display Performance Status
-a) $0–2999 - Poor
-c) $3000–4999 - Average
-d) $5000–9999 - Good
-e) $10000–14999 - Excellent
-f) $15000 and above – Outstanding
-Note: Sales Amount and Commission can be decimal values ex.356.89
-
 */
 using System;
 
@@ -20,11 +10,9 @@ class Program {
     string name = Console.ReadLine();
     Console.WriteLine ("Enter Sales Amount");
     double sales = Convert.ToDouble(Console.ReadLine());
-
     //calculate the Commission: SALES COMMISSION = 200 + (0.09 * SALESAMOUNT)
     double comm = 200 + (0.09 * sales);
    Console.WriteLine ($"Sales commission for {name} is ${comm}");
-
    //$0–2999 - Poor
    if (comm <= 2999) {
    Console.WriteLine ("Performance is Poor");
@@ -40,7 +28,6 @@ class Program {
    //$15000 and above – Outstanding
    } else if (comm > 15000) {
    Console.WriteLine ("Performance is Outstanding");
-   }
-    
+   }   
   }
 }
